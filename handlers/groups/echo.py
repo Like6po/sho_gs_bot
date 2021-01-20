aa00d5000176a0ab63c12569db6a27410a6b9ea8
Like6po <42671486+Like6po@users.noreply.github.com>
2021-01-20 20:05:00 +0300

    fix

--- handlers/groups/echo.py
+++ handlers/groups/echo.py
@@ -1,4 +1,3 @@
-
 import subprocess
 
 import aiohttp
@@ -13,13 +12,29 @@ import os
 from data.config import WIT_TOKEN
 from loader import dp
 
-# Windows
-path = "C:\\Users\\Admin\\PycharmProjects\\crocodile_bot\\data\\voices\\"
-pathffmpeg = "C:\\Users\\Admin\\Desktop\\ffmpeg\\bin\\"
 
-# linux
-#path = "data\\voices"
-#pathffmpeg = ""
+# Windows
+
+
+def win_pathes(code_name):
+    path = "C:\\Users\\Admin\\PycharmProjects\\crocodile_bot\\data\\voices\\"
+    pathffmpeg = "C:\\Users\\Admin\\Desktop\\ffmpeg\\bin\\"
+    return {'path_1': f"{path}\\1_{code_name}.ogg",
+            'path_2': f"{path}\\2_{code_name}.ogg",
+            'path_ffmpeg': f"{pathffmpeg}ffmpeg",
+            'path_dir': f'{path}\\{code_name}\\',
+            'path_out': f'{path}\\{code_name}\\out_%03d.ogg'}
+
+
+def linux_pathes(code_name):
+    path = "./data/voices"
+    pathffmpeg = ""
+    return {'path_1': f"{path}/1_{code_name}.ogg",
+            'path_2': f"{path}/2_{code_name}.ogg",
+            'path_ffmpeg': f"{pathffmpeg}ffmpeg",
+            'path_dir': f'{path}/{code_name}/',
+            'path_out': f'{path}/{code_name}/out_%03d.ogg'}
+
 
 async def wit_speech(f):
     headers = {"authorization": f"Bearer {WIT_TOKEN}",
@@ -29,6 +44,7 @@ async def wit_speech(f):
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, data=f) as resp:
             info = await resp.json()
+
         return info
 
 
@@ -36,40 +52,51 @@ async def wit_speech(f):
                     content_types=ContentType.VOICE)
 async def voice(message: types.Message):
     msg = await message.reply(f'{hbold("Обработка...")}')
-    print(msg)
+
     code_name = f"{message.from_user.id}_{message.message_id}"
 
-    await message.voice.download(f'{path}\\1_{code_name}.ogg')
+    pathes = win_pathes(code_name)
+    #pathes = linux_pathes(code_name)
 
-    process = subprocess.run([f'{pathffmpeg}ffmpeg', '-i', f'{path}\\1_{code_name}.ogg',
-                              f'{path}\\2_{code_name}.ogg'])
+    await message.voice.download(pathes['path_1'])
+
+    process = subprocess.run([pathes['path_ffmpeg'], '-i', pathes['path_1'], pathes['path_2']],
+                             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if process.returncode != 0:
         return await msg.edit_text(f'{hbold("Не удалось распознать! (#1)")}')
 
-    os.mkdir(f'{path}\\{code_name}')
+    os.mkdir(pathes['path_dir'])
 
-
-    process = subprocess.run([f'{pathffmpeg}ffmpeg', '-i', f'{path}\\2_{code_name}.ogg',
+    process = subprocess.run([pathes['path_ffmpeg'], '-i', pathes['path_2'],
                               '-f', 'segment', '-segment_time', '14', '-c', 'copy',
-                              f'{path}\\{code_name}\\out_%03d.ogg'])
+                              pathes['path_out']], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     if process.returncode != 0:
         return await msg.edit_text(f'{hbold("Не удалось распознать! (#2)")}')
 
     text = ''
-    for file_name in os.listdir(f"{path}\\{code_name}"):
 
-        resp = await wit_speech(open(f'{path}\\{code_name}\\{file_name}', 'rb'))
+    dir_list = os.listdir(pathes['path_dir'])
+    dir_list.reverse()
+
+    i = 0
+
+    for file_name in dir_list:
+        i += 1
+        resp = await wit_speech(open(f"{pathes['path_dir']}{file_name}", 'rb'))
         try:
-            os.remove(f'{path}\\{code_name}\\{file_name}')
+            os.remove(f"{pathes['path_dir']}{file_name}")
         except:
             pass
-        text += resp['text']
+        print(resp)
+        text = resp['text'] + " " + text
+        if i < len(dir_list):
+            await msg.edit_text(text=f"{message.from_user.get_mention()} сказал:\n{text}...")
 
     await msg.edit_text(text=f"{message.from_user.get_mention()} сказал:\n{text}")
 
     try:
-        os.remove(f'{path}2_{code_name}.ogg')
-        os.remove(f'{path}1_{code_name}.ogg')
-        os.rmdir(f"{path}\\{code_name}")
+        os.remove(pathes['path_1'])
+        os.remove(pathes['path_2'])
+        os.rmdir(pathes['path_dir'])
     except:
         pass
